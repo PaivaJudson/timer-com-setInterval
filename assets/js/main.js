441ce36fe0d3fa1f55dd91e91e49aac043d116d5
judsonpaiva <judson.paiva@hotmail.com>
2022-08-07 16:01:07 +0100

    refactorando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,45 @@
-const relogio = document.querySelector(".relogio");
-const iniciar = document.querySelector(".iniciar");
-const pausar = document.querySelector(".pausar");
-const zerar = document.querySelector(".zerar");
-let segundos = 0;
-let timer;
+function relogio() {
+    const relogio = document.querySelector(".relogio");
+    let segundos = 0;
+    let timer;
 
-iniciar.addEventListener('click', function(e) {
-    iniciarRelogio();
-});
+    document.addEventListener('click', function(e) {
+        const elemento = e.target;
 
-pausar.addEventListener('click', function(e) {
-    clearInterval(timer);
-});
+        if (elemento.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0;
+        }
 
-zerar.addEventListener('click', function(e) {
-    relogio.innerHTML = '00:00:00';
-    segundos = 0;
-});
+        if (elemento.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
 
-
-function criarHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-br', {
-        hour12: false,
-        timeZone: 'GMT'
+        if (elemento.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciarRelogio();
+        }
     });
+
+
+    function criarHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-br', {
+            hour12: false,
+            timeZone: 'GMT'
+        });
+    }
+
+    function iniciarRelogio() {
+        timer = setInterval(function() {
+            segundos++;
+            relogio.innerHTML = criarHoraDosSegundos(segundos);
+        }, 1000);
+    }
 }
 
-function iniciarRelogio() {
-    timer = setInterval(function() {
-        segundos++;
-        relogio.innerHTML = criarHoraDosSegundos(segundos);
-    }, 1000);
-}
+relogio();
